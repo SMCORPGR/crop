26c8c054c2381637313f1f52b788ed6f260de33a
SMCORPGR <hr@successmetrics.io>
2025-08-03 07:42:58 -0700

    baseline

---
+++ force-app/main/default/aura/FlowOutput/FlowOutputController.js
@@ -0,0 +1,33 @@
+({
+   init : function (component) {
+      // Find the component whose aura:id is "flowData"
+      var flow = component.find("flowData");
+      // In that component, start your flow. Reference the flow's API Name.
+      flow.startFlow("digitalm__DM_New_Production_Interview");
+   },
+                                
+    
+    handleStatusChange : function (component, event) {
+      if(event.getParam("status") === "FINISHED") 
+      {
+         // Get the output variables and iterate over them
+         var outputVariables = event.getParam("outputVariables");
+         console.log(outputVariables); 
+         var outputVar;
+         for(var i = 0; i < outputVariables.length; i++) 
+         {
+            outputVar = outputVariables[i];
+            console.log(outputVar);
+            // Pass the values to the component's attributes
+            if(outputVar.name === "recordId") 
+            console.log(outputVar.name);    
+            {
+               var urlEvent = $A.get("e.force:navigateToSObject");
+               urlEvent.setParams({"recordId": outputVar.value});
+               urlEvent.fire();
+               $A.get('e.force:refreshView').fire();
+            } 
+         }
+        }
+   },
+})
